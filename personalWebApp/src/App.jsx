04b19ebf7b17fb77d7906ab23a0e66752a028a99
Etea <eteaisme@gmail.com>
2024-02-21 18:23:43 -0500

    Removed unused styling and added main content

--- personalWebApp/src/App.jsx
+++ personalWebApp/src/App.jsx
@@ -2,7 +2,22 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1></h1>
+      <h1>Elliot Tam</h1>
+      <h2>Personal Web App</h2>
+      <br />
+      <br />
+      <br />
+      <p>
+        Hi!👋 My name is Elliot Tam and I am a student at Western Tech.
+        <br />
+        Some of my hobbies are programming, robotics and volleyball. This is a
+        web application I built for my computer science class.
+      </p>
+      <br />
+      <br />
+      <footer>
+        You can check out my Github or reach me at me email address.
+      </footer>
     </>
   );
 }
