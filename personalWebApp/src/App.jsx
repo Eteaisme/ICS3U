f5e70fed68660c56b9fd14404d938dc7cc35fa40
Etea <eteaisme@gmail.com>
2024-02-21 19:00:45 -0500

    Added a footer and tab class

--- personalWebApp/src/App.jsx
+++ personalWebApp/src/App.jsx
@@ -18,6 +18,22 @@ function App() {
       <footer>
         You can check out my Github or reach me at me email address.
       </footer>
+      <br />
+      <a href="https://github.com/Eteaisme" target="_blank">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+          alt="My Github"
+          width={50}
+        />
+      </a>
+      <br />
+      <br />
+      <footer>
+        <a href="mailto:elliot.tam@student.tdsb.on.ca">
+          Elliot.Tam@student.tdsb.on.ca
+        </a>
+        <span className="tab">February 21st 2024</span>
+      </footer>
     </>
   );
 }
