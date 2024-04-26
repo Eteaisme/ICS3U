f19ebf18bb50393196f5cda52a28614c347e5e81
Eteaisme <eteaisme@gmail.com>
2024-04-26 15:11:42 -0400

    Added person class

--- Java-Projects/Java-Functions/javaCustomFunctions/src/App.java
+++ Java-Projects/Java-Functions/javaCustomFunctions/src/App.java
@@ -1,5 +1,7 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+public static void main(String[] args) {
+   Person Joe = new Person("Joe", 40);
+   System.out.println(Joe.StudentAge()); 
+}    
 }
